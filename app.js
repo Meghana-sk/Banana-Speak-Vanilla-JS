f9aa68b2ac2fb5d492a216f21e5c82da7a64d005
MeghanaSK <meghana.meghanask.sk@gmail.com>
2020-11-28 12:11:19 +0530

    imported js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// console.log("Hello people!")
+
+// input
+var username = prompt("Give me your name");
+
+// processing
+var welcomeMsg = "This script works " + username;
+
+// output
+alert(welcomeMsg);
