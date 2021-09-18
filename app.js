9a11291b1c0c63d9a7601154715bfe8de74693e0
Meghana <meghana.meghanask.sk@gmail.com>
2021-09-19 00:26:40 +0530

    style: Removed commented code

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-
-// // console.log("Hello people!")
-
-// // input
-// var username = prompt("Give me your name");
-
-// // processing
-// var welcomeMsg = "This script works " + username;
-
-// // output
-// alert(welcomeMsg);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var divOutput = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
-
-//divOutput.innerText = "is awesome"; // to write a text
-//console.log(divOutput);
-
-//console.log(txtInput);
 
 function getTranslationURL(text){
     return serverURL + "?" +"text=" + text;
@@ -31,10 +14,6 @@ function errorHandler(error){
 }
 
 function clickEventHandler() {
-    // console.log("clicked");
-    // console.log("input",txtInput.value); //to read a value/text
-   // divOutput.innerText = "ASNJDSFGSJFJ" + txtInput.value;
-
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
@@ -46,10 +25,4 @@ function clickEventHandler() {
     .catch(errorHandler);
 }
 
-
-
-
 btnTranslate.addEventListener("click",clickEventHandler);
-
-
-
